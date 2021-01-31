9f761894a961f9f29e3ca5645af3405d0340c9ed
Bjorn Yourey <byourey7783@gmail.com>
2021-01-31 17:21:28 -0500

    added 5 day forecast blocks

--- script.js
+++ script.js
@@ -1,89 +1,99 @@
-var currentDay = document.querySelector('.date');
-currentDay.textContent = dayjs().format('MM/D/YYYY');
+const currentDay = dayjs().format('MM/DD/YYYY');
+ $("#currentDay").text(currentDay);
 
-var todayDate = document.querySelector('.dateNow');
-todayDate.textContent = dayjs().format('MM/D/YYYY');
+let lat = document.querySelector('.lat');
+let lon = document.querySelector('.lon');
 
-var todayDate = document.querySelector('.dateNow');
-// d = date.getDate(),
-// m = date.getMonth(),
-// y = date.getFullYear();
-
-
-// for(i=0; i < 5; i++){
-// var curdate = new Date(y, m, d+i)
-// console.log(curdate)
-// }
-
-
-
-
-
-// $('.btn').click(function(){
-
-//     var city = $('.inputValue').val();
-//     var key = '88da1611665dfe9338cd6679dee95466';
-
-//     $.ajax({
-//         url: 'http://api.openweathermap.org/data/2.5/weather',
-//         dataType: 'json',
-//         type: 'GET',
-//         data: {q:city, appid: key, units: 'metric'},
-
-//         success: function(data) {
-//             var temp = '';
-//             $.each(data.weather, function(index, val) {
-//                wf += '<p><b>' + data.name + "</b><img src="+ val.icon + ".png></p>"+
-//                data.main.temp + '&deg;F ' + ' | ' + val.main + ", " + 
-//                val.humidity + val.wind + val.uv
-//             },
-//             $("showWeather").html(temp));
-
-//         }
-//     });
-    
-// });
+// Function to display current forecast
 
 $('.d-flex').on('submit', function(e) {
     e.preventDefault();
     var city = $('.inputValue').val();
     var key = '88da1611665dfe9338cd6679dee95466';
+    
   
     $.ajax({
-      url: 'https://api.openweathermap.org/data/2.5/weather',
+      url: 'https://api.openweathermap.org/data/2.5/weather?',
       dataType: 'json',
       type: 'GET',
       data: {
         q: city,
         appid: key,
-        units: 'metric'
+        units: 'imperial'
       },
       success: function(data) {
-        let html = data.weather.map(loc => `<p><b>${data.name}</b><img src="${loc.icon}.png" /></p>${data.main.temp}&deg;F | ${loc.main}, ${data.main.humidity}%, ${data.wind.speed} @ ${data.wind.deg}&deg;`);
+
+        let html = data.weather.map(loc => `${data.name}<img src="${loc.icon}.png" />
+         </p>Temperature:${data.main.temp}°F | ${loc.main}, 
+         </p>Humidity:${data.main.humidity}%, 
+         </p>Wind Speed:${data.wind.speed}MPH,
+         </p>UV Index:${data.main.uvi}`);
+         
         $("#showWeather").html(html);
       }
     });
   });
 
 
-// // $('.btn').click(function(){
-//     $.ajax({
-//         type: "GET",
-//         url: 'https://api.openweathermap.org/data/2.5/weather?q=' + city +  '&appid=' + keys,
-//         // makeUrl(url, keys), // Gets the constructed url
-//         dataType: "json",
-//         // contentType: 'application/json',
-//         success: function(data) {
-//             // if(city === -1) set the index to negative -1
-//                 //What to do on success 
-//         }
-//     });
-// // });
+// Save user inputs to local storage
+  var searchHistory = (localStorage.searchHistory) ? JSON.parse(localStorage.searchHistory) : [];
+document.querySelector(".btn").addEventListener("click", () => {
+  searchHistory.push(document.querySelector(".inputValue").value);
+  localStorage.searchHistory = JSON.stringify(searchHistory);
+});
+document.querySelector(".inputValue").addEventListener("focus", () => {
+  var data = document.querySelector("datalist#searchdata");
+  data.innerHTML = "";
+  searchHistory.forEach((search) => {
+    data.innerHTML = "<option>" + data.innerHTML;
+    data.querySelector("option").innerText = search;
+  });
+});
+
+// To get 5 day forecast
+const dateNow = document.querySelector('.fiveDayForecast .dateNow');
+dateNow.innerText = dayjs().add(1,'day').format('MM/DD/YYYY')
+const dateNow1 = document.querySelector('.fiveDayForecast .dateNow1');
+dateNow1.innerText = dayjs().add(2,'day').format('MM/DD/YYYY')
+const dateNow2 = document.querySelector('.fiveDayForecast .dateNow2');
+dateNow2.innerText = dayjs().add(3,'day').format('MM/DD/YYYY')
+const dateNow3 = document.querySelector('.fiveDayForecast .dateNow3');
+dateNow3.innerText = dayjs().add(4,'day').format('MM/DD/YYYY')
+const dateNow4 = document.querySelector('.fiveDayForecast .dateNow4');
+dateNow4.innerText = dayjs().add(5,'day').format('MM/DD/YYYY')
+
+$('.tempDay').on('submit', function(e) {
+    e.preventDefault();
+    // var city = $('.inputValue1').val();
+    var keys = 'd00b3725186bba9d9c7acbf2d2ce36e2';  
+  
+  
+    $.ajax({
+      url: 'https://api.openweathermap.org/data/2.5/onecall?',
+      dataType: 'json',
+      type: 'GET',
+      data: {        
+        lat: lat,
+        lon: lon,
+        appid: keys,
+        // units: 'imperial'
+      },
+      success: function(response) {
+          
+        let html = response.weather.map(loc => `${response.name}<img src="${loc.icon}.png" />
+        </p>Temperature:${response.main.temp}°F | ${loc.main}, 
+        </p>Humidity:${response.main.humidity}`)%
+       $("#showWeather1").html(html);
+     }
+   });
+ });
+
+      
 
 
 
-// // card , title , image, and these will be variables and use jquery to save them for each day for 5 days
-// // use a for loop 5 TimeRanges, create variables for temp and humidity and date
+
+
 
 
 
